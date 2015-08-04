a77126db9ab41f83f3345bd1a9c41f1b39cda7e3
boombox93 <taylor.engels@gmail.com>
2015-08-03 21:53:13 -0400

    Aug 3 2015 - creature sizing and improved movement

--- src/Launcher.java
+++ src/Launcher.java
@@ -4,7 +4,7 @@ public class Launcher
 
 	public static void main(String[] args) 
 	{
-		Game game = new Game("Title", 400, 400);
+		Game game = new Game("Title", 800, 800);
 		game.start();
 	}
 
--- src/Player.java
+++ src/Player.java
@@ -8,7 +8,7 @@ public class Player extends Creature
 
 	public Player(Game game, float x, float y) 
 	{
-		super(x, y);
+		super(x, y, Creature.DEFAULT_CREATURE_WIDTH, Creature.DEFAULT_CREATURE_HEIGHT);
 		_game = game;
 		
 	}
@@ -16,30 +16,41 @@ public class Player extends Creature
 	@Override
 	public void tick()
 	{
+		getInput();
+		move();
+	}
+	
+	private void getInput()
+	{
+		xMove = 0;
+		yMove = 0;
+		
 		if(_game.getKeyManager().up)
 		{
-			y -= 3;
+			yMove = -speed;
+			
 		}
 		if(_game.getKeyManager().down)
 		{
-			y += 3;
+			yMove = speed;
+			
 		}
 		if(_game.getKeyManager().left)
 		{
-			x -= 3;
+			xMove = -speed;
+			
 		}
 		if(_game.getKeyManager().right)
 		{
-			x += 3;
+			xMove = speed;
+			
 		}
-		
-		
 	}
 
 	@Override
 	public void render(Graphics g) 
 	{
-		g.drawImage(Assets.player, (int) x, (int) y, null);
+		g.drawImage(Assets.player, (int) x, (int) y, width, height, null);
 		
 	}
 
